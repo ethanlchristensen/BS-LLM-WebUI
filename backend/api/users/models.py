ef0fda839c338a5f69e2d10cadf9d1555d582515
ethanlchristensen <ethanlchristensen@outlook.com>
2024-09-03 22:02:43 -0500

    updates

---
+++ backend/api/users/models.py
@@ -0,0 +1,21 @@
+from django.contrib.auth.models import User
+from django.db import models
+from PIL import Image
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default="profile_pictures/default.jpg", upload_to="profile_pictures")
+    prefered_model = models.CharField(max_length=256, default="")
+
+    def __str__(self):
+        return f"{self.user.username}'s Profile"
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
