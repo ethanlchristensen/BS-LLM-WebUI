f662ccdd56e0df5efe98d683210517a08a682013
ethanlchristensen <ethanlchristensen@outlook.com>
2024-10-11 23:53:21 -0500

    updates

--- frontend/src/features/model/api/get-models.ts
+++ frontend/src/features/model/api/get-models.ts
@@ -1,11 +1,11 @@
 import { useQuery } from "@tanstack/react-query";
 import { api } from '@/lib/api-client';
-import { Model } from '@/types/api';
+import { BaseModelEntity } from '@/types/api';
 import Cookies from 'js-cookie';
 
 export const useGetModelsQuery = () => {
     return useQuery({
-        queryFn: async (): Promise<Model[]> => {
+        queryFn: async (): Promise<BaseModelEntity[]> => {
             return api.get(`/ollama/models/`, {
                 headers: { Authorization: `Token ${Cookies.get('token')}` }
             });
