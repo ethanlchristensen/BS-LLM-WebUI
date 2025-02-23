72f8d7f6192b73ecb6ac6ebb02ce62f4487c1e4e
ethanlchristensen <ethanlchristensen@outlook.com>
2025-02-23 03:05:09 -0600

    [update] - update suggestion model to llama3.2 (for better results?)

--- frontend-v2/src/features/suggestions/api/get-three-suggestions.tsx
+++ frontend-v2/src/features/suggestions/api/get-three-suggestions.tsx
@@ -1,22 +1,16 @@
 import { api } from "@/lib/api-client";
 import { Suggestions } from "@/types/api";
-import Cookies from "js-cookie";
 import { useQuery } from "@tanstack/react-query";
 
 export const useGetSuggestionsQuery = (count: number) => {
   return useQuery({
-    // Adding count to the queryKey ensures that the cache correctly differentiates between queries with different counts
     queryKey: ["suggestions", count],
     queryFn: async (): Promise<Suggestions | null> => {
-      return api.post(
-        "/suggestions/",
-        { provider: "ollama", model: "phi4:latest", count: count },
-        {
-          headers: {
-            Authorization: `Token ${Cookies.get("token")}`,
-          },
-        },
-      );
+      return api.post("/suggestions/", {
+        provider: "ollama",
+        model: "llama3.2:latest",
+        count: count,
+      });
     },
     staleTime: 1000 * 60 * 100,
   });
